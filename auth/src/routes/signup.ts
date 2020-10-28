baa783c2bffd8e657fd401cc51511dfaa7fbd2ff
Njaal Wiik <njaal.wiik@gmail.com>
2020-10-28 08:46:03 +0100

    Update signup.ts

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
-import { DatabaseConnectionError } from '../errors/DatabaseConnectionError';
-import { RequestValidationError } from '../errors/RequestValidationError';
+import { DatabaseConnectionError } from '../errors/databaseConnectionError';
+import { RequestValidationError } from '../errors/requestValidationError';
 
 const router = express.Router();
 
