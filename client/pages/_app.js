2c5dcd0512ecaa817cec04420a6be24a719200ec
Njaal Wiik <njaal.wiik@gmail.com>
2020-11-02 10:34:41 +0100

    Issues with Custom App GetInitialProps

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/buildClient';
 
-export default ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
+
+AppComponent.getInitialProps = async context => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+
+  return data;
+};
+
+export default AppComponent;
